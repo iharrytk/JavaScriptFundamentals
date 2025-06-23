c0a62a892555e0e10f5791aa44ee7499095430bc
Haritha Thiagu Kumar <haritha.vf130@gmail.com>
2025-06-23 00:35:54 -0500

    filter function scenario

--- filterfunctionpractice.js
+++ filterfunctionpractice.js
@@ -1,3 +1,15 @@
 let numbers=[1,2,3,4,5,6,7,8,9,10]
 let evennumbers=numbers.filter(numbers=>numbers%2===0);
 console.log(evennumbers);
+
+
+
+
+let testcases=[{testcaseid:1,testresult:'pass'},{testcaseid:2,testresult:'fail'},
+    {testcaseid:3,testresult:'pass'},{testcaseid:4,testresult:'fail'},
+{testcaseid:5,testresult:'pass'},{testcaseid:6,testresult:'fail'},
+{testcaseid:7,testresult:'pass'},{testcaseid:8,testresult:'fail'},
+{testcaseid:9,testresult:'pass'},{testcaseid:10,testresult:'fail'}]
+
+let failedtestcases=testcases.filter(testcases=>testcases.testresult==='fail');
+console.log(failedtestcases)
