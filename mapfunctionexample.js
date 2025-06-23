13fa795d72decda467510464e53d7771a03e4772
Haritha Thiagu Kumar <haritha.vf130@gmail.com>
2025-06-22 23:52:13 -0500

    map filter reduce examples

---
+++ mapfunctionexample.js
@@ -0,0 +1,10 @@
+let numbers=[2,4,6];
+let doubled=numbers.map(num=>num*2);
+console.log(doubled);
+
+
+
+
+let users=['user1','user2'];
+let testdata=users.map(users=>({username:users,password:'Test@123'}));
+console.log(testdata)
